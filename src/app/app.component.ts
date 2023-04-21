4bb127fc9ab2fbf4565fc7ea38fef6f7fc6b43bd
Fabio Murtas <murtasfabio@gmail.com>
2023-04-21 02:42:55 +0200

    secondo commit components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div class="hello">Sesso {{gender==='m'? 'Uomo':'Donna'}}</div>`,
+  template: `
+  <button (click)="visible=!visible">invert</button>
+  <button (click)="toggle()">toggle</button>
+  <h1 *ngIf="visible"> Hello World</h1>
+  <li
+    [hidden]="!visible"
+    *ngFor="let user of users"
+  >{{user}}</li>
+   `,
   styles: [`.hello { background-color : red }`]
 })
 export class AppComponent {
-  title = 'myapp';
-  gender ='m';
+  visible = true
+  users = ['fabio', 'lorenzo', 'Simone']
+  toggle() {
+      this.visible = !this.visible
+  }
 }
